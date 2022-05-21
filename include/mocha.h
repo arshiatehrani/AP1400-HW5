685ea44a6bdfc3ab45098becd821b1ab1f9ba9f0
arshiatehrani <arshia.tehrani1380@gmail.com>
2022-05-21 15:56:22 +0430

    Mocha

--- include/mocha.h
+++ include/mocha.h
@@ -1,4 +1,23 @@
 #ifndef MOCHA_H
 #define MOCHA_H
+#include "espresso_based.h"
+#include "ingredient.h"
+#include "sub_ingredients.h"
+#include <iostream>
+class Mocha : EspressoBased {
+public:
+    Mocha() = default;
+    Mocha(const Mocha& cap);
+    ~Mocha();
+    void operator=(const Mocha& cap);
 
+    virtual std::string get_name();
+    virtual double price();
+
+    void add_side_item(Ingredient* side);
+    std::vector<Ingredient*>& get_side_items();
+
+private:
+    std::vector<Ingredient*> side_items;
+};
 #endif // MOCHA_H
--- src/mocha.cpp
+++ src/mocha.cpp
@@ -1,1 +1,48 @@
 #include "mocha.h"
+
+Mocha::Mocha(const Mocha& cap)
+{
+    std::cout << "Mocha Copy Constructor" << std::endl;
+    *this = cap;
+}
+
+Mocha::~Mocha()
+{
+    for (const auto& i : side_items)
+        delete i;
+    side_items.clear();
+}
+
+void Mocha::operator=(const Mocha& cap)
+{
+    std::cout << "Mocha operator=" << std::endl;
+    this->name = cap.name;
+    for (const auto& ptr_ing : cap.ingredients)
+        this->ingredients.push_back(ptr_ing);
+
+    for (const auto& ptr_side_ing : cap.side_items)
+        this->side_items.push_back(ptr_side_ing);
+}
+
+std::string Mocha::get_name()
+{
+    return this->name;
+}
+
+double Mocha ::price()
+{
+    double price { static_cast<double>(Espresso(2).get_units() * Espresso(2).get_price_unit() + Milk(2).get_units() * Milk(2).get_price_unit() + MilkFoam(2).get_units() * MilkFoam(2).get_price_unit()) };
+    for (const auto& ptr_side_ing : side_items)
+        price += ptr_side_ing->get_units() * ptr_side_ing->get_price_unit();
+    return price;
+}
+
+void Mocha::add_side_item(Ingredient* side)
+{
+    side_items.push_back(side);
+}
+
+std::vector<Ingredient*>& Mocha::get_side_items()
+{
+    return side_items;
+}
