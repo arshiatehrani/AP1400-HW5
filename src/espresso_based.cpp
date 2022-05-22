e4ee24b044fcb0f2c59bc510df0518d078929341
arshiatehrani <arshia.tehrani1380@gmail.com>
2022-05-22 20:50:29 +0430

    brew() demo2 for Cappuccino & Mocha

--- src/espresso_based.cpp
+++ src/espresso_based.cpp
@@ -14,136 +14,131 @@ EspressoBased::~EspressoBased()
 
 void EspressoBased ::brew()
 {
+    using namespace ftxui;
     using namespace std::chrono_literals;
-    auto box1 = ftxui::dbox({ ftxui::text(this->get_name()) | ftxui::bgcolor(ftxui::Color::Red) | ftxui::bold | ftxui::center | ftxui::border });
-    auto screenbox1 = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(box1));
-    ftxui::Render(screenbox1, box1);
-    screenbox1.Print();
 
-    std::queue<std::string> q;
-    for (auto i : this->get_ingredients())
-        q.push(i->get_name());
+    std::queue<std::string> ing;
+    for (const auto& ing_it : this->get_ingredients())
+        ing.push(ing_it->get_name());
 
-    for (auto& i : this->get_ingredients()) {
+    // Top Box containing the name of coffee
+    auto box_top { dbox({ text(this->get_name()) | bgcolor(Color::DarkRed) | bold | center | border }) };
+    auto screen_box_top { Screen::Create(Dimension::Full(), Dimension::Fit(box_top)) };
+    Render(screen_box_top, box_top);
+    screen_box_top.Print();
 
-        auto screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(box1));
+    // Drawing the Ingredients
+    for (const auto& ing_it : this->get_ingredients()) {
+
+        auto screen = Screen::Create(Dimension::Full(), Dimension::Fit(box_top));
         std::string reset_position;
 
+        // Each Ingredient
         for (double percentage { 0.0f }; percentage <= 1.01f; percentage += 0.007f) {
-            std::string String_percent = std::to_string(int(percentage * 100)) + "/100";
+            std::string String_percent { std::to_string(int(percentage * 100)) + "/100" };
 
-            if (q.empty())
+            if (ing.empty())
                 break;
 
-            auto x = i->get_name();
+            auto ing_name { ing_it->get_name() };
 
-            // std::setw(5);
-            // std::right;
-            // std::cout << std::setw(10);
-            // auto y = this->get_ingredients();
+            if (ing.front() == "Cinnamon") {
 
-            if (q.front() == "Cinnamon") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(123, 63, 0)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(123, 63, 0)),
-                    ftxui::text(" " + String_percent) });
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(123, 63, 0)),
+                    gauge(percentage) | color(Color::RGB(123, 63, 0)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "Chocolate") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(92, 51, 23)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(92, 51, 23)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "Chocolate") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(92, 51, 23)),
+                    gauge(percentage) | color(Color::RGB(92, 51, 23)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "Sugar") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(244, 238, 232)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(244, 238, 232)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "Sugar") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(244, 238, 232)),
+                    gauge(percentage) | color(Color::RGB(244, 238, 232)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "Cookie") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(157, 126, 103)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(157, 126, 103)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "Cookie") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(157, 126, 103)),
+                    gauge(percentage) | color(Color::RGB(157, 126, 103)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "Espresso") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(60, 34, 24)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(60, 34, 24)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "Espresso") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(60, 34, 24)),
+                    gauge(percentage) | color(Color::RGB(60, 34, 24)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "Milk") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(255, 255, 255)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(255, 255, 255)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "Milk") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(255, 255, 255)),
+                    gauge(percentage) | color(Color::RGB(255, 255, 255)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "MilkFoam") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(219, 168, 88)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(219, 168, 88)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "MilkFoam") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(219, 168, 88)),
+                    gauge(percentage) | color(Color::RGB(219, 168, 88)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
-            } else if (q.front() == "Water") {
-                auto filling = ftxui::hbox({ // ftxui::hbox({ftxui::text(x)}) | ftxui::border | ftxui::center ,
-                    ftxui::text(x + " :  \t") | ftxui::color(ftxui::Color::RGB(128, 197, 222)),
-                    ftxui::gauge(percentage) | ftxui::color(ftxui::Color::RGB(128, 197, 222)),
-                    ftxui::text(" " + String_percent) });
+                std::this_thread::sleep_for(0.05s);
+
+            } else if (ing.front() == "Water") {
+
+                auto filling { hbox({ text(ing_name + " :  \t") | color(Color::RGB(128, 197, 222)),
+                    gauge(percentage) | color(Color::RGB(128, 197, 222)),
+                    text(" " + String_percent) }) };
                 Render(screen, filling);
                 std::cout << reset_position;
                 screen.Print();
                 reset_position = screen.ResetPosition();
-                std::this_thread::sleep_for(0.005s);
-                // q.pop();
+                std::this_thread::sleep_for(0.05s);
             }
         }
-
-        q.pop();
+        ing.pop();
     }
     std::string reset_position;
-    // auto b2 = ftxui::dbox({ ftxui::text("Your " + this->get_name() + " is ready. ENJOY!!") | ftxui::color(ftxui::Color::Green) | ftxui::bold | ftxui::center });
-    // auto screenb2 = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(b2));
-    // ftxui::Render(screenb2, b2);
-    // screenb2.Print();
-    auto box2 = ftxui::dbox({ ftxui::text("DONE!!!111!!") | ftxui::bgcolor(ftxui::Color::Green) | ftxui::bold | ftxui::center | ftxui::border });
-    auto screenbox2 = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(box2));
-    ftxui::Render(screenbox2, box2);
-    screenbox2.Print();
+
+    auto box_bottom { ftxui::dbox({ ftxui::text("DONE!!!111!!") | ftxui::bgcolor(ftxui::Color::Green) | ftxui::bold | ftxui::center | ftxui::border }) };
+    auto screen_box_bottom { ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Fit(box_bottom)) };
+    ftxui::Render(screen_box_bottom, box_bottom);
+    screen_box_bottom.Print();
 }
 
 const std::vector<Ingredient*>& EspressoBased ::get_ingredients() const
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include "cappuccino.h"
 #include "espresso_based.h"
+#include "ftxui/dom/canvas.hpp"
+#include "ftxui/dom/flexbox_config.hpp"
+#include "ftxui/dom/node.hpp"
+#include "ftxui/screen/box.hpp"
+#include "ftxui/screen/color.hpp"
+#include "ftxui/screen/screen.hpp"
+#include "ftxui/screen/terminal.hpp"
+#include "ftxui/util/ref.hpp"
 #include "ingredient.h"
 #include "mocha.h"
 #include "sub_ingredients.h"
@@ -12,13 +20,108 @@ int main(int argc, char** argv)
     if (false) // make false to run unit-tests
     {
         // debug section
-        Cappuccino cappuccino;
-        cappuccino.add_side_item(new Water { 1 });
-        Cappuccino equal;
-        equal.add_side_item(new Sugar { 1 });
-        equal = cappuccino;
-        std::cout << "equal: " << equal.price() << "cappuccino: " << cappuccino.price() << std::endl;
-        std::cout << "equal: " << equal.get_ingredients().size() << "cappuccino: " << cappuccino.get_ingredients().size() << std::endl;
+        using namespace ftxui;
+        using namespace std::chrono_literals;
+
+        std::string reset_position;
+        for (float percentage = 0.0f; percentage <= 1.0f; percentage += 0.002f) {
+            std::string data_downloaded = std::to_string(int(percentage * 100)) + "% / 100%";
+            auto document = hbox({
+                text("downloading:"),
+                gauge(percentage) | flex,
+                text(" " + data_downloaded),
+            });
+            auto screen = Screen(100, 1);
+            Render(screen, document);
+            std::cout << reset_position;
+            screen.Print();
+            reset_position = screen.ResetPosition();
+
+            std::this_thread::sleep_for(0.02s);
+        }
+        std::cout << std::endl;
+        // std::string reset_position2;
+        // for (float percentage = 0.0f; percentage <= 1.0f; percentage += 0.002f) {
+        //     std::string data_downloaded = std::to_string(int(percentage * 5000)) + "/5000";
+        //     auto document = hbox({
+        //         text("downloading:"),
+        //         gauge(percentage) | flex,
+        //         text(" " + data_downloaded),
+        //     });
+        //     auto screen = Screen(100, 1);
+        //     Render(screen, document);
+        //     std::cout << reset_position2;
+        //     screen.Print();
+        //     reset_position2 = screen.ResetPosition();
+
+        //     std::this_thread::sleep_for(0.01s);
+        // }
+        // std::cout << std::endl;
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // using namespace ftxui;
+        // using namespace std::chrono_literals;
+
+        // std::string reset_position;
+        // for (float percentage = 0.0f; percentage <= 1.0f; percentage += 0.002f) {
+        //     std::string data_downloaded = std::to_string(int(percentage * 5000)) + "/5000";
+
+        //     auto gauge_up = //
+        //         hbox({
+        //             vtext("gauge vertical"),
+        //             separator(),
+        //             gaugeUp(percentage),
+        //         })
+        //         | border;
+
+        //     auto gauge_down = //
+        //         hbox({
+        //             vtext("gauge vertical"),
+        //             separator(),
+        //             gaugeDown(percentage),
+        //         })
+        //         | border;
+
+        //     auto gauge_right = //
+        //         vbox({
+        //             text("gauge horizontal"),
+        //             separator(),
+        //             gaugeRight(percentage),
+        //         })
+        //         | border;
+
+        //     auto gauge_left = //
+        //         vbox({
+        //             text("gauge horizontal"),
+        //             separator(),
+        //             gaugeLeft(percentage),
+        //         })
+        //         | border;
+
+        //     auto document = hbox({
+        //         gauge_up,
+        //         filler(),
+        //         vbox({
+        //             gauge_right,
+        //             filler(),
+        //             text(data_downloaded) | border | center,
+        //             filler(),
+        //             gauge_left,
+        //         }),
+        //         filler(),
+        //         gauge_down,
+        //     });
+
+        //     auto screen = Screen(32, 16);
+        //     Render(screen, document);
+        //     std::cout << reset_position;
+        //     screen.Print();
+        //     reset_position = screen.ResetPosition();
+
+        //     std::this_thread::sleep_for(0.01s);
+        // }
+        // std::cout << std::endl;
+        ////////////////////////////////////////////////////////////////////////////////
 
     } else {
         ::testing::InitGoogleTest(&argc, argv);
