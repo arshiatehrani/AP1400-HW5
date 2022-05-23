15edcc566903ff511b1de8b9cbdc6a3cdc89b9d0
arshiatehrani <arshia.tehrani1380@gmail.com>
2022-05-23 14:30:19 +0430

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,26 @@ int main(int argc, char** argv)
     if (false) // make false to run unit-tests
     {
         // debug section
+        using namespace ftxui;
+        using namespace std::chrono_literals;
 
+        std::string reset_position;
+        for (float percentage = 0.0f; percentage <= 1.0f; percentage += 0.002f) {
+            std::string data_downloaded = std::to_string(int(percentage * 5000)) + "/5000";
+            auto document = hbox({
+                text("downloading:"),
+                gauge(percentage) | flex,
+                text(" " + data_downloaded),
+            });
+            auto screen = Screen(100, 1);
+            Render(screen, document);
+            std::cout << reset_position;
+            screen.Print();
+            reset_position = screen.ResetPosition();
+
+            std::this_thread::sleep_for(0.01s);
+        }
+        std::cout << std::endl;
     } else {
         ::testing::InitGoogleTest(&argc, argv);
         std::cout << "RUNNING TESTS ..." << std::endl;
